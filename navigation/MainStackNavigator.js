725e47226580eada8e7126537e5bb36d2d63e2fe
TrinhVanHau1408 <hauaclas@gmail.com>
2023-05-04 20:58:49 +0700

    add hidden header stack

--- navigation/MainStackNavigator.js
+++ navigation/MainStackNavigator.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import  { Test, SignUp, Login }  from "../views";
+import { Test, SignUp, Login } from "../views";
 
 const Stack = createNativeStackNavigator();
 
 const MainStackNavigator = () => (
-    <Stack.Navigator initialRouteName="Login" >
-      <Stack.Screen name="Home" component={ Test }  />
-      <Stack.Screen name="Thai" component={ SignUp }  />
-       <Stack.Screen name="Login" component={ Login }  />
-    </Stack.Navigator>
-  );
+  <Stack.Navigator
+    initialRouteName="Login"
+    screenOptions={{
+      headerShown: false
+    }} >
+    <Stack.Screen name="Home" component={Test} />
+    <Stack.Screen name="Thai" component={SignUp} />
+    <Stack.Screen name="Login" component={Login} />
+  </Stack.Navigator>
+);
 
 
 export { MainStackNavigator };
