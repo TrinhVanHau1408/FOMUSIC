710e39978c0c5c556139f6acf044e6fd28cb1731
ngochan <20520484@gm,uit.edu.vn>
2023-05-08 09:23:28 +0700

    fix icon

--- constants/icons.js
+++ constants/icons.js
@@ -50,5 +50,6 @@ export default {
     playlistAdd: require('../assets/images/add-to-list.png'),
     save: require('../assets/images/icon_save.png'),
     save2: require('../assets/images/icon_save2.jpg'),
-    greyAvt: require('../assets/images/grey_avt.png')
+    greyAvt: require('../assets/images/grey_avt.png'),
+    cameraAddPhoto : require('../assets/images/camera-add-photo.png')
 }
