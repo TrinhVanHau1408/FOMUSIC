fa0934d744d68609c584dce83180d4628f048558
TrinhVanHau1408 <hauaclas@gmail.com>
2023-05-13 22:32:33 +0700

    connect firebase

---
+++ firebase/connectDB.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQqhnghG8ZgbWenPzjPoB1FZPrnlJStd0",
+  authDomain: "fomusicapp.firebaseapp.com",
+  projectId: "fomusicapp",
+  storageBucket: "fomusicapp.appspot.com",
+  messagingSenderId: "356559607343",
+  appId: "1:356559607343:web:2e1a4dfb2947956b2f98da"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
