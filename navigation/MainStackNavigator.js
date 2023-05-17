71559734060d84fee5765ae458f03a0adaf4df4d
ngochan <20520484@gm,uit.edu.vn>
2023-05-18 04:07:08 +0700

    change MainStack

--- navigation/MainStackNavigator.js
+++ navigation/MainStackNavigator.js
@@ -48,16 +48,18 @@ const MainStackNavigator = ({ navigation }) => (
 
     {/* <Stack.Screen name="Home" component={Home} /> */}
     
-    {/* <Stack.Screen name="Register" component={Register} />
+    <Stack.Screen name="Register" component={Register} />
+
     <Stack.Screen name="Login" component={Login} />
-    <Stack.Screen name="Playing" component={Playing} />
+
+    {/* <Stack.Screen name="Playing" component={Playing} />
     <Stack.Screen name="PlayingMore" component={PlayingMore} />
     <Stack.Screen name="PlayingFullLyric" component={PlayingFullLyric} />
     <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
     <Stack.Screen name="VerifyAccount" component={VerifyAccount} />
     <Stack.Screen name="NewPassword" component={NewPassword} />
     <Stack.Screen name="Library" component={Library} />
-    <Stack.Screen name="Artist" component={Artist} /> */}
+    <Stack.Screen name="Artist" component={Artist} />  */}
 
     {/* <Stack.Screen name="Playlist" component={Playlist} />
     <Stack.Screen name="DetailPlaylist" component={DetailPlaylist} />
@@ -80,7 +82,7 @@ const MainStackNavigator = ({ navigation }) => (
     {/* <Stack.Screen name="InterfaceStyle" component={InterfaceStyle} /> */}
     {/* <Stack.Screen name="Notifications" component={Notifications} /> */}
 
-    <Stack.Screen name="Home3" component={Home3} />
+    {/* <Stack.Screen name="Home3" component={Home3} /> */}
 
 
   </Stack.Navigator>
