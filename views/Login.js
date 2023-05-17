b00962e7446b7e684b20bb7f1896a1bfef8bec5f
TrinhVanHau1408 <hauaclas@gmail.com>
2023-05-17 21:09:43 +0700

    xu ly login

--- views/Login.js
+++ views/Login.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, Image, ScrollView } from 'react-native';
+
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import HeaderSign from '../components/header/HeaderSign';
 import FooterSign from '../components/footer/FooterSign';
 import MyInput from '../components/misc/MyInput';
@@ -7,15 +8,16 @@ import { icons } from '../constants';
 import MyButton from '../components/misc/MyButton';
 import OrLine from '../components/misc/OrLine';
 import MyNavigation from '../components/misc/MyNavigation';
-
-
-const LogIn = ({ navigation }) => {
-  const [username, setUsername] = useState('');
+import { auth, signInWithEmailAndPassword, firebaseDatabaseRef, get, child, firebaseDatabase } from '../firebase/connectDB';
+// import { saveDataAsyncStorage } from '../untiles/AsyncStorage';
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [isFormatEmail, setIsFormatEmail] = useState(false)
+  const [isSign, setIsSign] = useState(false)
   const handleNavigatorRegister = () => {
     navigation.navigate('Register');
-   
+
   }
 
   const handleNavigatorForgotPassword = () => {
@@ -26,8 +28,56 @@ const LogIn = ({ navigation }) => {
     navigation.goBack();
   }
   const handleLogin = () => {
-    navigation.navigate('Home1');
+
+    var validRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!validRegex.test(email)) {
+      setIsFormatEmail(true)
+    } else {
+      setIsFormatEmail(false)
+    }
+    // var validRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // if (!validRegex.test(email)) {
+    //   setIsFormatEmail(true)
+    // } else {
+    //   setIsFormatEmail(false)
+    // }
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        if (user) {
+          const dbRef = firebaseDatabaseRef(firebaseDatabase);
+
+          // Lấy dữ liệu user từ firebase
+          get(child(dbRef, `fomusic/users/${user.id}`))
+            .then((snapshot) => {
+              if (snapshot.exists()) {
+                const id = snapshot.key;
+                const displayName = snapshot.val().displayName;
+                const imgUrl = snapshot.val().imgUrl;
+                const currUser = {
+                  id: id,
+                  displayName: displayName,
+                  imgUrl: imgUrl ? imgUrl : null
+                }
+
+                saveDataAsyncStorage("user", currUser); ư
+                navigation.navigate('App');
+              }
+            })
+        }
+      }).catch((err) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if (errorCode == 'auth/wrong-password' || errorCode == 'auth/user-not-found') {
+          setIsSign(true)
+        } else {
+          setIsSign(false)
+        }
+        console.log(errorCode)
+      })
   };
+
 
   return (
     <ScrollView style={styles.container}>
@@ -35,11 +85,15 @@ const LogIn = ({ navigation }) => {
         <HeaderSign title={'Login'} goBack={goBack} />
       </View>
       <View style={styles.contentContainer}>
-        <MyInput icon={icons.mail} placeholder={'Email'} />
-        <MyInput icon={icons.lock} placeholder={'Password'} />
+        {isSign && <Text style={{ textAlign: 'center', fontSize: 13, color: 'red' }}>Tên đăng nhập hoặc mật khẩu không đúng!</Text>}
+        <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
 
+          <MyInput icon={icons.mail} placeholder={'Email'} setState={setEmail} valueState={email} />
+          {isFormatEmail && <Text style={{ textAlign: 'center', fontSize: 13, color: 'red' }}>Vui lòng nhập lại email - sai cú pháp!</Text>}
+        </View>
+        <MyInput icon={icons.lock} placeholder={'Password'} setState={setPassword} valueState={password} />
         <View style={{ marginTop: 36 }}>
-          <MyButton title={'Login'}  handleNavigator={handleLogin}/>
+          <MyButton title={'Login'} handleNavigator={handleLogin} />
         </View>
         <View style={styles.TextContainer}>
           <TouchableOpacity
@@ -61,7 +115,8 @@ const LogIn = ({ navigation }) => {
 
     </ScrollView>
   );
-};
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -107,4 +162,3 @@ const styles = StyleSheet.create({
   }
 });
 
-export default LogIn;
