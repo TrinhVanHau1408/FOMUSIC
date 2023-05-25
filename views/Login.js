5f9e6203365757d77513ca3e41ec53b3b0f4f388
TrinhVanHau1408 <hauaclas@gmail.com>
2023-05-25 08:34:44 +0700

    fix handle login

--- views/Login.js
+++ views/Login.js
@@ -1,6 +1,8 @@
 
 import React, { useState } from 'react'
-import { StyleSheet, View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView, Alert } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { logIn } from '../redux/slices/authSlice';
 import HeaderSign from '../components/header/HeaderSign';
 import FooterSign from '../components/footer/FooterSign';
 import MyInput from '../components/misc/MyInput';
@@ -8,13 +10,16 @@ import { icons } from '../constants';
 import MyButton from '../components/misc/MyButton';
 import OrLine from '../components/misc/OrLine';
 import MyNavigation from '../components/misc/MyNavigation';
-import { auth, signInWithEmailAndPassword, firebaseDatabaseRef, get, child, firebaseDatabase } from '../firebase/connectDB';
-import { saveDataAsyncStorage } from '../untiles/AsyncStorage';
+
+
 export default function Login({ navigation }) {
+  const dispatch = useDispatch();
+  const { loading, error } = useSelector((state) => state.auth);
+ 
   const [email, setEmail] = useState('tvh140802@gmail.com');
   const [password, setPassword] = useState('123456');
-  const [isFormatEmail, setIsFormatEmail] = useState(false)
-  const [isSign, setIsSign] = useState(false)
+  // const [isFormatEmail, setIsFormatEmail] = useState(false)
+  // const [isSign, setIsSign] = useState(false)
   const handleNavigatorRegister = () => {
     navigation.navigate('Register');
 
@@ -27,73 +32,18 @@ export default function Login({ navigation }) {
   const goBack = () => {
     navigation.goBack();
   }
+
+
   const handleLogin = () => {
+    console.log("Clicked button login")
 
-    var validRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!validRegex.test(email)) {
-      setIsFormatEmail(true)
-    } else {
-      setIsFormatEmail(false)
+    dispatch(logIn({ email, password }));
+
+    if (!loading && error == null) {
+      navigation.navigate('App');
     }
-
-    console.log(`${email} + ${password}`)
-    // signInWithEmailAndPassword(auth, email, password)
-    //   .then((userCredential) => {
-    //     // Signed in 
-    //     const user = userCredential.user;
-    //     debugger
-    //     if (user) {
-    //       console.log('oker', user.id)
-    //     }
-    //     // ...
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //   });
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        if (user) {
-          const dbRef = firebaseDatabaseRef(firebaseDatabase);
-          console.log('Logining', user.uid)
-          // Lấy dữ liệu user từ firebase
-          get(child(dbRef, `users/${user.uid}`))
-            .then((snapshot) => {
-              if (snapshot.exists()) {
-                console.log(snapshot.val().displayName)
-                const id = snapshot.key;
-                const displayName = snapshot.val().displayName;
-                const imgUrl = snapshot.val().imgUrl;
-                const currUser = {
-                  id: id,
-                  displayName: displayName,
-                  imgUrl: imgUrl ? imgUrl : null
-                }
-                console.log('Saving')
-                let isSaved = saveDataAsyncStorage('user', currUser);
-                if (isSaved) {
-                  navigation.navigate('App');
-                } else {
-                  console.log("Login - Error saving user");
-                }
-
-              }
-            })
-
-        }
-      }).catch((err) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        if (errorCode == 'auth/wrong-password' || errorCode == 'auth/user-not-found') {
-          setIsSign(true)
-        } else {
-          setIsSign(false)
-        }
-        console.log(errorCode)
-      })
-  };
-
+    
+  }
 
   return (
     <ScrollView style={styles.container}>
@@ -101,11 +51,11 @@ export default function Login({ navigation }) {
         <HeaderSign title={'Login'} goBack={goBack} />
       </View>
       <View style={styles.contentContainer}>
-        {isSign && <Text style={{ textAlign: 'center', fontSize: 13, color: 'red' }}>Tên đăng nhập hoặc mật khẩu không đúng!</Text>}
+
         <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
 
           <MyInput icon={icons.mail} placeholder={'Email'} setState={setEmail} valueState={email} />
-          {isFormatEmail && <Text style={{ textAlign: 'center', fontSize: 13, color: 'red' }}>Vui lòng nhập lại email - sai cú pháp!</Text>}
+
         </View>
         <MyInput icon={icons.lock} placeholder={'Password'} setState={setPassword} valueState={password} />
         <View style={{ marginTop: 36 }}>
@@ -124,10 +74,6 @@ export default function Login({ navigation }) {
           <MyNavigation title='Don’t have an account?' textButton={'Register'} handleNavigator={handleNavigatorRegister} />
         </View>
       </View>
-
-      {/* <View style={styles.footerContainer}>
-        <FooterSign />
-      </View> */}
 
     </ScrollView>
   );
