c3adf0b8c50d02da613e3fb43a55456c5fb0afbd
TrinhVanHau1408 <hauaclas@gmail.com>
2023-05-25 08:41:57 +0700

    no change

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from "@react-navigation/native";
-import { MainStackNavigator } from './navigation/StackNavigator';
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import { ForgotPassword, Login, NewPassword, Register, VerifyAccount, Wellcome } from './views';
 import { MainTabNavigator } from './navigation/MainTabNavigator';
 import Playing from './views/Playing';
@@ -11,42 +11,26 @@ const Stack = createNativeStackNavigator();
 function App() {
 
   return (
-    // <AuthProvider>
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Login'
-        screenOptions={{
-          headerShown: false
-        }}
-      >
-        <Stack.Screen name='Init' component={Wellcome} />
-        <Stack.Screen name='Playing' component={Playing} />
-        <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Register' component={Register} />
-        <Stack.Screen name='VerifyAccount' component={VerifyAccount} />
-        <Stack.Screen name='NewPassword' component={NewPassword} />
-        <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
-        <Stack.Screen name='App' component={MainTabNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    // </AuthProvider>
-
+    <Provider store={store}>
+      <NavigationContainer >
+        <Stack.Navigator
+          initialRouteName='Init'
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <Stack.Screen name='Init' component={Wellcome} />
+          <Stack.Screen name='Playing' component={Playing} />
+          <Stack.Screen name='Login' component={Login} />
+          <Stack.Screen name='Register' component={Register} />
+          <Stack.Screen name='VerifyAccount' component={VerifyAccount} />
+          <Stack.Screen name='NewPassword' component={NewPassword} />
+          <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
+          <Stack.Screen name='App' component={MainTabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-{/* <Stack.Navigator
-        initialRouteName='Login'
-        screenOptions={{
-            headerShown: false
-        }}
-    >
-        <Stack.Screen name='Init' component={Wellcome} />
-        <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Register' component={Register} />
-        <Stack.Screen name='VerifyAccount' component={VerifyAccount} />
-        <Stack.Screen name='NewPassword' component={NewPassword} />
-        <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
-        <Stack.Screen name='App' component={MainTabNavigator} />
-    </Stack.Navigator> */}
 
 export default App;
