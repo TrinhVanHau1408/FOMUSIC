5b7b0a321a6514c984138348a76187605414908d
TrinhVanHau1408 <hauaclas@gmail.com>
2023-06-02 21:25:01 +0700

    fix paylistSlice

--- redux/slices/playlistsSlice.js
+++ redux/slices/playlistsSlice.js
@@ -1,36 +1,16 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { firebaseDatabaseRef, get, child, firebaseDatabase } from '../../firebase/connectDB';
-
-export const getPlayLists = createAsyncThunk('playlists/getPlayLists', async ({ }, { rejectWithValue }) => {
-    try {
-        console.log('useruid createAsyncThunk')
-        const dbRef = firebaseDatabaseRef(firebaseDatabase);
-        const responeSnapshot = await get(child(dbRef, 'playlists'));
-        // console.log(responeSnapshot.val())
-        // // const user = {
-        // //     displayName: responeSnapshot.val().displayName,
-        // //     email: responeSnapshot.val().email,
-        // //     uid: responeSnapshot.key,
-        // //     imgUrl: responeSnapshot.val().imgUrl,
-        // //     emailVerified: responeSnapshot.val().emailVerified,
-        // //     typeUser: responeSnapshot.val().typeUser,
-        // // }
-        return responeSnapshot.val()
-    } catch (error) {
-        console.log('error', error.message)
-        return rejectWithValue(error.message);
-    }
-})
+import { readDataFirebaseWithChildCondition } from '../../firebase/controllerDB';
 
 // Lấy tất cả các playlist của user hiện tại.
-export const getAllPlaylistByUserId = createAsyncThunk('song/getAllSongByUseId', async ({ userId }, {dispatch, rejectWithValue }) => {
+export const getAllPlaylistByUserId = createAsyncThunk('song/getAllSongByUseId',
+    async ({ userId }, { dispatch, rejectWithValue }) => {
 
-    console.log('getAllSongByUserId: ', userId)
+        console.log('getAllSongByUserId:', userId)
 
-    const playlist = await readDataFirebaseWithChildCondition('playlists', 'userId', userId);
-    console.log('playlist:', playlist)
-    dispatch(setPlayLists(playlist));
-})
+        const playlist = await readDataFirebaseWithChildCondition('playlists', 'userId', userId);
+        console.log('playlist:', playlist)
+        dispatch(setPlayLists(playlist));
+    })
 
 
 const playlistsSlice = createSlice({
