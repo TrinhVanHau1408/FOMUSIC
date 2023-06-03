7b0efc5c701a6e9ee5b76ad0cd7075758a56d45d
TrinhVanHau1408 <hauaclas@gmail.com>
2023-06-04 00:37:17 +0700

    fix return function filterObject

--- utilities/Object.js
+++ utilities/Object.js
@@ -1,14 +1,16 @@
 
- /**
- * Filter object by field value.
- *
- * @param {Object} objectFilter - Object need filter.
- * @param {String} fieldFilter - Field need filter.
- * @param {String} valueFilter - Value need filter.
- * @returns {Array} Array - [key, {}]}.
- */
+/**
+* Filter object by field value.
+*
+* @param {Object} objectFilter - Object need filter.
+* @param {String} fieldFilter - Field need filter.
+* @param {String} valueFilter - Value need filter.
+* @returns {Array} Array - [ {}, {}, ...].
+*/
 export const filterObject = (objectFilter, fieldFilter, valueFilter) => {
-    const filteredObjects = Object.entries(objectFilter).filter(
-        ([key, obj]) => obj[fieldFilter] === valueFilter);
-    return filteredObjects;
+    const mapObjects = Object.entries(objectFilter)
+    .map(([key, val]) => ({key, ...val}))
+    // .filter(item => item[fieldFilter] === valueFilter)
+    
+    return mapObjects;
 }
