c55718fe6ca1bd0b631ca21415331b6a032a5bc1
ngochan <20520484@gm,uit.edu.vn>
2023-06-06 03:48:16 +0700

    add slice artist

--- redux/slices/artistSlice.js
+++ redux/slices/artistSlice.js
@@ -1,9 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import {firebaseDatabaseRef, get, child, firebaseDatabase } from '../../firebase/connectDB';
+import { firebaseDatabaseRef, get, child, firebaseDatabase } from '../../firebase/connectDB';
+import { equalTo, orderByChild, ref } from 'firebase/database';
 
-export const getArtist = createAsyncThunk('artist/getArtist', async({artist},{ rejectWithValue })=> {
+export const getArtist = createAsyncThunk('artist/getArtist', async ({ artist }, { rejectWithValue }) => {
     try {
-        console.log('useruid createAsyncThunk' , artist)
+        console.log('useruid createAsyncThunk', artist)
         const dbRef = firebaseDatabaseRef(firebaseDatabase);
         const responeSnapshot = await get(child(dbRef, `artists/${artist}`));
         // const user = {
@@ -21,35 +22,70 @@ export const getArtist = createAsyncThunk('artist/getArtist', async({artist},{ r
     }
 })
 
+
+// Truy vấn tất cả các nghệ sĩ đã theo dõi
+export const queryFollowedArtists = createAsyncThunk('followedArtist/queryFollowedArtists',
+    async ({ userId }, { dispatch, rejectWithValue }) => {
+        try {
+            const dbRef = firebaseDatabaseRef(firebaseDatabase);
+            const snapshot = await get(orderByChild(child(dbRef, 'artists'), 'follows/' + userId), equalTo('', 'value'));
+
+            // Lấy giá trị của tất cả các nghệ sĩ đã theo dõi
+            const followedArtists = snapshot.val();
+
+            // Kiểm tra nếu không có nghệ sĩ nào đã theo dõi
+            if (!followedArtists) {
+                console.log('Không có nghệ sĩ nào đã theo dõi!');
+                return;
+            }
+            dispatch(setFollowedArtist(followedArtists))
+
+            // Lặp qua từng nghệ sĩ đã theo dõi và truy cập thông tin của họ
+            // Object.keys(followedArtists).forEach((artistId) => {
+            //     const artist = followedArtists[artistId];
+            //     console.log('Thông tin nghệ sĩ:', artist);
+            // });
+        } catch (error) {
+            console.log('Lỗi truy vấn:', error);
+        }
+        
+    }
+)
+
+
 const artistSlice = createSlice({
     name: 'artist',
     initialState: {
         artist: null,
+        followedArtist: null,
         loading: false,
         error: null,
-      },
+    },
     reducers: {
         setArtist: (state, action) => {
             state.artist = action.payload;
+        },
+        setFollowedArtist: (state, action) => {
+            state.followedArtist = action.payload;
         }
     },
     extraReducers: (builder) => {
         // getUserUid
-        builder.addCase(getArtist.pending, (state) => {
+        builder.addCase(queryFollowedArtists.pending, (state) => {
             state.loading = true;
             console.log("getArtistUid pending")
         })
-        builder.addCase(getArtist.fulfilled, (state, action) => {
+        builder.addCase(queryFollowedArtists.fulfilled, (state, action) => {
             state.loading = false;
-            state.user = action.payload;
+            // state.artist = action.payload;
             console.log("getArtistUid fulfilled")
         })
-        builder.addCase(getArtist.rejected, (state, action) => {
+        builder.addCase(queryFollowedArtists.rejected, (state, action) => {
             state.loading = false;
             state.error = action.error.message;
             console.log("getArtistUid rejected")
         })
     }
 })
-export const { setArtist } = artistSlice.actions;
+export const { setArtist, setFollowedArtist } = artistSlice.actions;
 export default artistSlice.reducer;
