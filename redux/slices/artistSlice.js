35c81404fb49c5b99da58529ec5a254769d9477d
TrinhVanHau1408 <hauaclas@gmail.com>
2023-06-10 21:37:54 +0700

    add function getArtistByUserId

--- redux/slices/artistSlice.js
+++ redux/slices/artistSlice.js
@@ -1,78 +1,68 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { readDataFirebase, readDataFirebaseWithChildCondition } from '../../firebase/controllerDB';
 import { firebaseDatabaseRef, firebaseDatabase, get, orderByChild, equalTo, query } from '../../firebase/connectDB';
-export const getArtist = createAsyncThunk('artist/getArtist', async ({ artist }, { rejectWithValue }) => {
+export const getArtistByUserId = createAsyncThunk('artist/getArtistByUserId', async ({ userId }, {dispatch, rejectWithValue }) => {
     try {
-        console.log('useruid createAsyncThunk', artist)
-        const dbRef = firebaseDatabaseRef(firebaseDatabase);
-        const responeSnapshot = await get(child(dbRef, `artists/${artist}`));
-        // const user = {
-        //     displayName: responeSnapshot.val().displayName,
-        //     email: responeSnapshot.val().email,
-        //     uid: responeSnapshot.key,
-        //     imgUrl: responeSnapshot.val().imgUrl,
-        //     emailVerified: responeSnapshot.val().emailVerified,
-        //     typeUser: responeSnapshot.val().typeUser,
-        // }
-        return responeSnapshot.val();
+        const resArtist = await readDataFirebase(`artists/${userId}`);
+        dispatch(setArtist(resArtist));
     } catch (error) {
-        console.log('error', error.message)
-        return rejectWithValue(error.message);
+        dispatch(setArtist(null));
+      
     }
 })
 
 
 // Truy vấn tất cả các nghệ sĩ đã theo dõi
-export const getFollowedArtists = createAsyncThunk('artist/getFollowedArtists',
-    async ({ userId }, { dispatch, rejectWithValue }) => {
-        console.log("getFollowedArtists");
+// export const getFollowedArtists = createAsyncThunk('artist/getFollowedArtists',
+//     async ({ userId }, { dispatch, rejectWithValue }) => {
+//         console.log("getFollowedArtists");
 
-        // const artistsRef = firebaseDatabaseRef(firebaseDatabase, 'artists');
-        // console.log(`follows/${userId}`)
-        // const queryRef = query(artistsRef, orderByChild(userId+'/active'), equalTo(""));
+//         // const artistsRef = firebaseDatabaseRef(firebaseDatabase, 'artists');
+//         // console.log(`follows/${userId}`)
+//         // const queryRef = query(artistsRef, orderByChild(userId+'/active'), equalTo(""));
 
-        // try {
-        //   const snapshot = await get(queryRef);
-        //   const artists = snapshot.val();
-        //   console.log("artists",artists);
-        // } catch (error) {
-        //   console.log("Lỗi khi truy vấn dữ liệu:", error);
-        //   return null;
-        // }
+//         // try {
+//         //   const snapshot = await get(queryRef);
+//         //   const artists = snapshot.val();
+//         //   console.log("artists",artists);
+//         // } catch (error) {
+//         //   console.log("Lỗi khi truy vấn dữ liệu:", error);
+//         //   return null;
+//         // }
 
-        // const as =await readDataFirebase('artists');
-        // console.log(as)
+//         // const as =await readDataFirebase('artists');
+//         // console.log(as)
 
-        const artistsRef = firebaseDatabaseRef(firebaseDatabase, 'artists');
+//         const artistsRef = firebaseDatabaseRef(firebaseDatabase, 'artists');
 
-        // Tạo query với orderByChild và equalTo
-        const followsQuery = query(
-            firebaseDatabaseRef(artistsRef, "follows"),
-            orderByChild('active'),
-            equalTo(false)
-        );
+//         // Tạo query với orderByChild và equalTo
+//         const followsQuery = query(
+//             firebaseDatabaseRef(artistsRef, "follows"),
+//             orderByChild('active'),
+//             equalTo(false)
+//         );
 
-        // Truy vấn dữ liệu
-        get(followsQuery)
-            .then((snapshot) => {
-                console.log(snapshot)
-                if (snapshot.exists()) {
+//         // Truy vấn dữ liệu
+//         get(followsQuery)
+//             .then((snapshot) => {
+//                 console.log(snapshot)
+//                 if (snapshot.exists()) {
                   
-                    snapshot.forEach((childSnapshot) => {
-                        const followId = childSnapshot.key;
-                        const followData = childSnapshot.val();
+//                     snapshot.forEach((childSnapshot) => {
+//                         const followId = childSnapshot.key;
+//                         const followData = childSnapshot.val();
 
-                        // Xử lý dữ liệu theo ý muốn
-                        console.log(`Follow: ${followId}, Active: ${followData.active}`);
-                    });
-                }
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+//                         // Xử lý dữ liệu theo ý muốn
+//                         console.log(`Follow: ${followId}, Active: ${followData.active}`);
+//                     });
+//                 }
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
 
-    }
-)
+//     }
+// )
 
 
 const artistSlice = createSlice({
